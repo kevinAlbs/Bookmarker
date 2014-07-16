051d25b065f0b7754d0445a6cc509b5a7365b2ff
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2014-07-15 22:07:49 -0400

    Shadow DOM

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,41 @@
+var drawer_html = null;
+var fetching = false, fetch_callbacks = [];
+
+/* either fetches html, or if already fetching, adds callback */
+function fetchHtml(callback){
+	if (callback) {
+		fetch_callbacks.push(callback);
+	}
+	if(fetching){
+		//just add callback, return
+		return;
+	}
+	fetching = true;
+	$.ajax({
+	    url: chrome.extension.getURL("drawer.html"),
+	    dataType: "html",
+	    success: function(data){
+	    	drawer_html = data;
+	    	for(var i = 0; i < fetch_callbacks.length; i++){
+	    		fetch_callbacks[i]();
+	    	}
+	    	fetching = false;
+	    }
+	});
+}
+
+
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if (request.msg == "fetch_url"){
 			var t = sender.tab;
 			sendResponse({url: t.url});//pretty strange that the tab can't even access it's own url
+		} else if(request.msg == "fetch_html"){
+			if(drawer_html !== null){
+				sendResponse({html: drawer_html});
+			} else {
+				fetchHtml(function(){sendResponse({html: drawer_html});});
+			}
 		}
 });
+fetchHtml();
--- extension/main.js
+++ extension/main.js
@@ -1,5 +1,5 @@
 var API_ROOT = "http://localhost/bookmarks/back-end/index.php/";
-var bRoot = $("<div id='bm_extension_bookmark'><h1>Bookmark Page</h1><input type='text' placeholder='Notes'/><div>Press <span>&lt;enter&gt;<span> to save<br/>Press <span class='red'>&lt;esc&gt;</span> to exit</div>");
+var shadow_root = null, shadow_content = null;
 var qCount = 0;
 var canSave = false;
 var shiftPressed = false;
@@ -17,19 +17,13 @@ var keys = {
 }
 
 function reset(){
-	bRoot.find("input").val("");
-	bRoot.css({
-		background: "#000"
-	});
+	shadow_content.querySelector("input").value = "";
+	shadow_content.style.background = "#FFFFFF";
 	data.url = "";
 	data.title = "";
 	data.notes = "";
 	qCount = 0;
-
 }
-
-bRoot.find("h1").html($("title").html());
-$("body").append(bRoot);
 
 $(document).on("keydown", function(e){
 	if(e.keyCode == keys.HOLD){
@@ -37,6 +31,9 @@ $(document).on("keydown", function(e){
 	}
 });
 $(document).on("keyup", function(e){
+	if(shadow_root == null){
+		return;
+	}
 	//console.log(e.keyCode);
 	if(e.keyCode == keys.SHOW && shiftPressed){
 		qCount++;
@@ -49,18 +46,16 @@ $(document).on("keyup", function(e){
 		if(e.keyCode == keys.SAVE && canSave){
 			//save
 			canSave = false;
-			data.notes = $("#bm_extension_bookmark input").val().trim();
-			data.title = $("title").html();
-			bRoot.css({
-				background: "#97FF7A"
-			});
+			data.notes = shadow_content.querySelector("input").value.trim();
+			data.title = document.title;
+			shadow_content.style.background = "#97FF7A";
   			$.ajax({
   				url: API_ROOT + "bookmark/save",
   				method: "post",
   				data: data,
   				success: function(){
   					console.log("Saved");
-  					bRoot.fadeOut(250, reset);
+  					$(shadow_root).slideUp(250, reset);
   				},
   				complete: function(data){
   					console.log(data);
@@ -70,18 +65,26 @@ $(document).on("keyup", function(e){
 		else if(e.keyCode == keys.EXIT){
 			//exit
 			canSave = false;
-			bRoot.fadeOut(250, reset);
+			$(shadow_root).slideUp(250, reset);
 		}
 	}
 
 	if(qCount >= 2){
 		qCount = 0;
 		canSave = true;
-		bRoot.fadeIn(250, function(){
-			bRoot.find("input").focus();
+		$(shadow_root).slideDown(250, function(){
+			shadow_content.querySelector("input").focus();
 		});
 		chrome.runtime.sendMessage({msg: "fetch_url"}, function(response) {
   			data.url = response.url;
 		});
 	}
+});
+
+chrome.runtime.sendMessage({msg: "fetch_html"}, function(response){
+	document.body.innerHTML += response.html;
+	var temp = document.querySelector("#bm_extension_bookmark");
+	shadow_root = document.querySelector("#bm_extension_shadow_root");
+	shadow_root.createShadowRoot().appendChild(document.importNode(temp.content, true));
+	shadow_content = document.querySelector("#bm_extension_shadow_root::shadow .content");
 });
