40b3fc4196412b2a0584c5b2d2d4ec47209d18e4
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2015-05-20 00:38:06 -0400

    Moving drawer to popup

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,5 @@
-var drawer_html = null;
 var fetching = false, fetch_callbacks = [];
+
 
 /*
 Installation initialization
@@ -18,45 +18,28 @@ chrome.runtime.onInstalled.addListener(function(details){
 	}
 });
 
-/* either fetches html, or if already fetching, adds callback */
-function fetchHtml(callback){
-	if (callback) {
-		fetch_callbacks.push(callback);
-	}
-	if(fetching){
-		//just add callback, return
-		return;
-	}
-	fetching = true;
-	$.ajax({
-	    url: chrome.extension.getURL("drawer.html"),
-	    dataType: "html",
-	    success: function(data){
-	    	drawer_html = data;
-	    	for(var i = 0; i < fetch_callbacks.length; i++){
-	    		fetch_callbacks[i]();
-	    	}
-	    	fetching = false;
-	    }
-	});
-}
-
 /* add the same methods as the PHP API for bookmarks and categories */
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		switch(request.msg){
-			case "fetch_url":
-				var t = sender.tab;
-				sendResponse({url: t.url});//pretty strange that the tab can't even access it's own url
+			case "get_current_page":
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+					if(tabs.length > 0){
+						var tab = tabs[0];
+						sendResponse({
+							url: tab.url,
+							title: tab.title
+						});
+					} else {
+						sendResponse({
+							url: "",
+							title: ""
+						})
+					}
+				});
+				return true;
 				break;
-			case "fetch_html":
-				if(drawer_html !== null){
-					sendResponse({html: drawer_html});
-				} else {
-					fetchHtml(function(){sendResponse({html: drawer_html});});
-				}
-				break;
-			case "save_url":
+			case "save_page":
 				//get credentials and server root
 				chrome.storage.sync.get({
 					server: "",
@@ -94,4 +77,3 @@ chrome.runtime.onMessage.addListener(
 				break;
 		}
 });
-fetchHtml();
--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,36 @@
-$("#save").on("click", function(e){
-  e.preventDefault();
-  //get current tab
-  chrome.tabs.query({"active":true}, function(tabs){
-    if(tabs.length == 0){
-      return;
-    }
-    var tab = tabs[0];
-    chrome.tabs.sendMessage(tab.id, {cmd: "open_drawer"});
-    window.close();
+var dom = {
+  title: $("input[name=title]"),
+  url: $(".url"),
+  notes: $("input[name=notes]"),
+  view_link : $("#view"),
+  save_area : $(".save_area"),
+  save_message: $(".save_area .save_message")
+};
+
+var state = "can_save"; //just_saved|already_saved|login
+
+function saveTransition(){
+  dom.save_area.animate({
+    height: "10px"
   });
-})
+  dom.save_message.fadeIn();
+}
 
 chrome.runtime.sendMessage({msg: "get_settings"}, function(response){
-  $("#view").attr("href", response.server + "front-end");
-})
+  dom.view_link.attr("href", response.server + "front-end");
+});
+
+chrome.runtime.sendMessage({msg: "get_current_page"}, function(response){
+  dom.title.val(response.title);
+  dom.url.html(response.url);
+});
+
+$(document).on("keypress", function(e){
+  if(e.keyCode == 13){
+    //if the current state is possible to save a bookmark, save
+    saveTransition();
+  }
+});
 
 $("#settings").attr("href", chrome.extension.getURL("options.html"));
+dom.notes.focus();
